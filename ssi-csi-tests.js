a4a37cca5c67febfc79d08e260fb19912475bef1
Duncan Cragg <fjord@cilux.org>
2010-03-25 15:31:13 +0000

    parse HTML as XML in tests and first bit of recursive content test

--- ssi-csi-tests.js
+++ ssi-csi-tests.js
@@ -3,6 +3,8 @@
 var sys = require('sys');
 var http = require('http');
 
+var xml2object = require('./lib/xml2object');
+
 var test = require('../fjord/simple-test');
 
 sys.puts('------------------ SSI and CSI Tests ---------------------');
@@ -13,11 +15,14 @@ var client = http.createClient(8080, "localhost");
 
 var headers = { "Host": "localhost:8080" };
 
+// -------------------------------------------------------------------
+
 var r=client.request("GET", "/a/b/c/owid-73c2-4046-fe02-7312.json", headers);
 
 r.addListener("response", function(response){
 
-// -------------------------------------------------------------------
+var contentType = response.headers["content-type"];
+test.isEqual("Content-Type is application/json", "application/json", contentType);
 
 var statusCode = response.statusCode;
 test.isEqual("Status is 200", 200, statusCode);
@@ -32,12 +37,11 @@ var cacheNotify = response.headers["cache-notify"];
 test.isEqual("Cache-Notify is http://localhost:8080/fjord/cache-notify",
              "http://localhost:8080/fjord/cache-notify", cacheNotify);
 
-// -------------------------------------------------------------------
-
 var body = "";
 response.setBodyEncoding("utf8");
 response.addListener("data", function(chunk){ body+=chunk; });
 response.addListener("end", function(){
+
 test.isEqual("Test Server returned expected JSON content",
              JSON.parse(body), {"tags":["x","y"]}
 );
@@ -48,8 +52,6 @@ var r=client.request("GET", "/u/owid-73c2-4046-fe02-7312.js?x=1324134", headers)
 
 r.addListener("response", function(response){
 
-// -------------------------------------------------------------------
-
 var contentType = response.headers["content-type"];
 test.isEqual("Content-Type is application/javascript", "application/javascript", contentType);
 
@@ -57,9 +59,26 @@ var body = "";
 response.setBodyEncoding("utf8");
 response.addListener("data", function(chunk){ body+=chunk; });
 response.addListener("end", function(){
+
 test.isEqual("Test Server returned expected Javascript content",
  body,
 "O(\n{\"owid\":\"owid-73c2-4046-fe02-7312\",\"refs\":{},\"outlinks\":{},\"etag\":1,\"content\":{\"tags\":[\"x\",\"y\"]}}\n);\n"
+);
+
+// -------------------------------------------------------------------
+
+var r=client.request("GET", "/a/b/c/owid-123-abc.json", headers);
+
+r.addListener("response", function(response){
+
+var body = "";
+response.setBodyEncoding("utf8");
+response.addListener("data", function(chunk){ body+=chunk; });
+response.addListener("end", function(){
+
+test.isEqual("Test Server returned expected JSON content",
+ body,
+"{\"wrapper\":\"owid-4ead-f007\",\"title\":\"Object Notation and UX Object Notation\",\"content\":{\"mml\":[\"Let's look at a small Object:\",\"|[ O(\\n { \\\"owid\\\": \\\"owid-123-456\\\",\\n \\\"content\\\": { \\\"wrapper\\\": \\\"owid-4ead-f007\\\",\\n \\\"title\\\": \\\"Introduction\\\",\\n \\\"content\\\": { \\\"mml\\\": [ \\n \\\"Welcome to JSON-Mash!\\\"\\n ] }\\n }\\n }\\n)\\n ]|\",\"/[... to be continued.]/\"]}}\n"
 );
 
 // -------------------------------------------------------------------
@@ -68,11 +87,14 @@ var client = http.createClient(8880, "localhost");
 
 var headers = { "Host": "localhost:8880" };
 
+// -------------------------------------------------------------------
+
 var r=client.request("GET", "/a/b/c/owid-123-abc.html", headers);
 
 r.addListener("response", function(response){
 
-// -------------------------------------------------------------------
+var contentType = response.headers["content-type"];
+test.isEqual("Content-Type is text/html", "text/html", contentType);
 
 var statusCode = response.statusCode;
 test.isEqual("Status is 200", 200, statusCode);
@@ -81,15 +103,67 @@ var body = "";
 response.setBodyEncoding("utf8");
 response.addListener("data", function(chunk){ body+=chunk; });
 response.addListener("end", function(){
-test.isEqual("Test Server returned expected HTML content",
-             body,
-             "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\">\n  <head>\n    <meta http-equiv=\"Content-Type\" content=\"application/xhtml+xml; charset=UTF-8\"/>\n    <link rel=\"stylesheet\"  type=\"text/css\"        href=\"/css/json-mash.css\" />\n    <link rel=\"stylesheet\"  type=\"text/css\"        href=\"/css/site.css\" />\n  </head>\n  <body>\n<table onmousedown=\"return linkdrop(this,'http://localhost:8080/u/owid-123-abc.js')\" class=\"mash-u-tbar\"><tr>\n<td class=\"mash-u-tbar-title\"><h1 class=\"mash-u-tbar-h1\">Object Notation and UX Object Notation</h1></td>\n<td class=\"mash-u-tbar-controls\"><div onmousedown=\"getObject('http://localhost:8080/u/owid-123-abc.js', true); return false;\" class=\"mash-u-reload\"><img src=\"/img/reload.png\" title=\"reload\" /></div>\n<div onmousedown=\"return linkpick(event, this,'http://localhost:8080/u/owid-123-abc.js')\" class=\"mash-u-dragdrop\"><img src=\"/img/linkto.png\" title=\"grab link (drop in a titlebar)\" /></div>\n<div onmousedown=\"document.location='http://localhost:8080/u/owid-123-abc.js'\" class=\"mash-u-seejson\"><img src=\"/img/jsonto.png\" title=\"view source\" /></div>\n<div onmousedown=\"setViewPoint(event, 'owid-123-abc'); return false; \" class=\"mash-u-setviewpoint\"><img src=\"/img/jumpto.png\" title=\"set viewpoint to this\" /></div>\n<div onmousedown=\"openClose(event, 'next', 'mash-u-tbar', true)\" class=\"mash-u-toggle\"><img src=\"/img/isopen.png\" title=\"open/close\" /></div>\n</td>\n</tr></table>\n<div class=\"mash-u-main\"><div class=\"mash-mml-wrapper\"><p class=\"mash-para\">Let's look at a small Object:</p>\n<p class=\"mash-para\"><pre> O(\n { &quot;owid&quot;: &quot;owid-123-456&quot;,\n &quot;content&quot;: { &quot;wrapper&quot;: &quot;owid-4ead-f007&quot;,\n &quot;title&quot;: &quot;Introduction&quot;,\n &quot;content&quot;: { &quot;mml&quot;: [ \n &quot;Welcome to JSON-Mash!&quot;\n ] }\n }\n }\n)\n </pre></p>\n<p class=\"mash-para\"><i>... to be continued.</i></p>\n</div></div>\n\n  </body>\n</html>\n"
-);
+
+xml2object.parseString(body, function(xmlobj) {
+
+test.isEqual("Header text set",
+              xmlobj.html.body.table.tr.td[0].h1.content,
+             "Object Notation and UX Object Notation");
+
+test.isEqual("MML text set",
+              xmlobj.html.body.div.div.p[0].content,
+             "Let\'s look at a small Object:");
+
+test.isEqual("MML pre-formatted text set",
+              xmlobj.html.body.div.div.p[1].pre.content,
+             " O(\n { \"owid\": \"owid-123-456\",\n \"content\": { \"wrapper\": \"owid-4ead-f007\",\n \"title\": \"Introduction\",\n \"content\": { \"mml\": [ \n \"Welcome to JSON-Mash!\"\n ] }\n }\n }\n)\n ");
+
+test.isEqual("MML text set",
+              xmlobj.html.body.div.div.p[2].i.content,
+              "... to be continued.");
+
+});
 
 // -------------------------------------------------------------------
 
+var r=client.request("GET", "/a/b/c/owid-456-789.html", headers);
+
+r.addListener("response", function(response){
+
+var body = "";
+response.setBodyEncoding("utf8");
+response.addListener("data", function(chunk){ body+=chunk; });
+response.addListener("end", function(){
+
+xml2object.parseString(body, function(xmlobj) {
+
+test.isEqual("Header text set",
+              xmlobj.html.body.table.tr.td[0].h1.content,
+             "Embedded Demo");
+
+test.isEqual("MML text set",
+              xmlobj.html.body.div.div.p[1].content,
+             "Here is a lazy-loaded Object graph:");
+
+test.isEqual("Links set (not!)",
+              xmlobj.html.body.div.div.a[0].getAttrs().href,
+             "http://localhost:8080/u/owid-111-222.js");
+
+test.isEqual("MML text set",
+              xmlobj.html.body.div.div.p[2].content,
+             "Here is a self-referring Object:");
+
+test.isEqual("Links set (not!)",
+              xmlobj.html.body.div.div.a[1].getAttrs().href,
+             "http://localhost:8080/u/owid-999-999.js");
+
+// -------------------------------------------------------------------
 test.summary();
 
+});
+
+}); }); r.close();
+}); }); r.close();
 }); }); r.close();
 }); }); r.close();
 }); }); r.close();
